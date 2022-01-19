6fb14a8ba32234a132811239eb2967469a34ac7e
BryceWren <wrenba@g.cofc.edu>
2022-01-19 11:39:26 -0500

    Completed Homework 1

--- assignments/hw1/hw1.py
+++ assignments/hw1/hw1.py
@@ -1,35 +1,54 @@
 """
-Name: <your name goes here – first and last>
+Name: <Bryce Wren>
 <ProgramName>.py
 
 Problem: <Brief, one or two sentence description of the problem that this program solves, in your own words.>
 
 Certification of Authenticity:
-<include one of the following>
+I certify that this assignment is entirely my own work
 I certify that this assignment is entirely my own work.
-I certify that this assignment is my own work, but I discussed it with: <Name(s)>
 """
 
 
 def calc_rec_area():
-    pass
+    length = eval(input("Enter the length: "))
+    width = eval(input("Enter the width: "))
+    area = length * width
+    print("Area =", area)
 
 
 def calc_volume():
-    pass
+    length = eval(input('Enter the length'))
+    width = eval(input('Enter the width'))
+    height = eval(input('Enter the height'))
+    volume = length * width * height
+    print("Volume", volume)
 
 
 def shooting_percentage():
-    pass
+    total_shots = (eval(input("Enter the player's total shots:")))
+    shots_made = (eval(input("Enter how many shots the player made:")))
+    shot_percentage = shots_made / total_shots * 100
+    print("Shooting Percentage:", shot_percentage, "%")
 
 
 def coffee():
-    pass
+    coffee_cost = 10.50
+    shipping_cost = .86
+    fixed_cost = 1.50
+    product = eval(input("How many pounds of coffee would you like?"))
+    total = product * coffee_cost + shipping_cost * 2 + fixed_cost
+    print("You're total is:", total)
+
+
 
 
 def kilometers_to_miles():
-    pass
+    user = eval(input("How many Kilometers did you travel"))
+    kilometer = 1.61
+    miles_traveled = user / kilometer
+    print("That's", miles_traveled, "miles")
 
 
-if __name__ == '__main__':
+if BryceWren == '__main__':
     pass
