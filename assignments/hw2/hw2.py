7dece821270c24273a9f6439b5e453dc7da0d10d
BryceWren <wrenba@g.cofc.edu>
2022-01-26 23:04:22 -0500

    Completed Homework #2

--- assignments/hw2/hw2.py
+++ assignments/hw2/hw2.py
@@ -13,24 +13,45 @@ import math
 
 
 def sum_of_threes():
-    pass
+    user_input = eval(input('What is the upper bound?'))
+    sum = 0
+    for i in range(3,user_input + 1,3):# loop starts at 3 but stops at the upper bound where the user inputs, then steps
+        # every 3
+        sum = (sum + i)
+    print("sum of threes is", sum)
 
 
 def multiplication_table():
-    pass
-
+    for i in range(1,11): # using a nested for loop
+        for j in range(1,11):
+            print(i * j, end = '\t') # multiplying both loops and spacing them out using \t
+        print()
 
 def triangle_area():
-    pass
+    a = eval(input('Enter side a length:'))
+    b = eval(input('Enter side b length:'))
+    c = eval(input('Enter side c length:'))
+    s = (a + b + c)/2
+    area = math.sqrt(s*(s-a)*(s-b)*(s-c))
+    print('area is:',area)
 
 
 def sum_squares():
-    pass
+    lower = eval(input('Enter lower range:'))
+    upper = eval(input('Enter upper range:'))
+    sum = 0
+    for i in range(lower, upper + 1):
+        sum = sum + (i**2)
+    print(sum)
 
 
 def power():
-    pass
-
+    base = eval(input('enter base:'))
+    exponent = eval(input('enter exponent:'))
+    exp = 1
+    for i in range(exponent): # the exponent is the number of times you loop
+        exp = exp * base # using multiplication because of what an exponent does by multiplying the base 3 times.
+    print(base, '^', exponent,'=',exp)
 
 if __name__ == '__main__':
     pass
