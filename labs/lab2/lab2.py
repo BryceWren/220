1a10711aabbad34345818cb31429f0ae06e4fc19
BryceWren <wrenba@g.cofc.edu>
2022-01-31 15:20:39 -0500

    Completed lab 2

--- labs/lab2/lab2.py
+++ labs/lab2/lab2.py
@@ -1,25 +1,18 @@
 import math
 def average():
     values = eval(input("Enter the values to be entered:"))
-    sum = 0
-    har_mean_a = 0 #accumulator
+    rms = 0
+    har_mean_a = 0 # accumulator
     geo_mean_a = 1
     for i in range(values):
         num = eval(input("Enter number:"))
-        sum = sum + num**2
+        rms = rms + num**2
         har_mean_a = har_mean_a + 1/num
         geo_mean_a = (geo_mean_a * num)
-    final_value = math.sqrt(sum / values)
+    final_value = math.sqrt(rms / values)
     har_mean_b = values / (har_mean_a)
-    geo_mean_b = (geo_mean_a)**1/values
-    print(final_value, har_mean_b, geo_mean_b)
+    geo_mean_b = geo_mean_a**(1/values)
+    print("the rms mean is:", final_value)
+    print("the harmonic mean is:", har_mean_b)
+    print("the geometric mean is:", geo_mean_b)
 
-
-def har_mean():
-    values = eval(input("Enter the values to be entered:"))
-    sum = 0
-    for i in range(values):
-        num = eval(input("Enter number:"))
-        sum = sum + 1/num
-    mean = values / sum
-    print(mean)
