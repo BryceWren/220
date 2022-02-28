efd47b5d33f9929bb31ae12d70289b1916fad385
BryceWren <wrenba@g.cofc.edu>
2022-02-28 15:09:59 -0500

    completed lab 6

--- notes/Homework test.py
+++ notes/Homework test.py
@@ -1,24 +1,27 @@
-import math
+from graphics import *
+
+def encode():
+    user = (input("enter a message:"))
+    message = (user.split())
+    key = eval(input("enter a key:"))
+    word = 0
+    num = ""
+    print(message)
+    for number in message: #['the', 'dog']
+        letter = (ord(number) + key) #gives a number then adds key to it
+        word = word + (letter) #keeps the numbers an integer so its easily deciphered later in loop
+        enc_let = (chr(letter)) #changes the new unicode number to a different letter
+        num = num + enc_let + " " # makes the letter a string so it is able to print
+    print(num)
+    print(ord(number))
+    print(message)
+#encode()
 
 
-def sequence():
-    term = eval(input("how many terms would you like?"))
-    for i in range(1, term + 1):
-        num = (i % 2)+ i - 1
-        print(num, end= " ")
-
-from math import *
 def test():
-    user = eval(input("how many terms to sum?"))
-    num = 0
-    approx = 1
-    for i in range(1, user + 1):
-        numerator = 1
-        denom = (1 // i) + 2
-        approx = approx * (numerator/denom)
-    approx_pi = 4 * approx + 2
-    accuracy = approx_pi - math.pi
-    print("pi approximation:", approx_pi)
-    print("accuracy:", accuracy)
+    user = (input("enter a message:"))
+    message = user.join("[]")
+    print(message)
+    print(type(message))
 
-#test()
+test()
