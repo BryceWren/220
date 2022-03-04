9f958fecbf2dca1e324802b6aba21285b35c304a
BryceWren <wrenba@g.cofc.edu>
2022-03-04 11:30:46 -0500

    Homework 7

---
+++ assignments/hw7/encryption.py
@@ -0,0 +1,23 @@
+def encode(message, key):
+    user = str(message)
+    key = eval(key)
+    encoded_string = ""
+    for character in user:
+        code_character = (ord(character) + key)
+        enc_let = (chr(code_character))
+        encoded_string = encoded_string + enc_let
+    return encoded_string
+
+def encode_better(message, key):
+    user = input(message)
+    key = input(key)
+    encoded_string = ""
+    for number in range(len(user)):
+        code_character = ord(user[number]) - 65
+        index_key = (number % len(key))
+        key_string = key[index_key]
+        key_number = ord(key_string) - 65
+        difference_value = (code_character + key_number) % 58
+        value_letter = difference_value + 65
+        encoded_string = encoded_string + chr(value_letter)
+    return encoded_string
