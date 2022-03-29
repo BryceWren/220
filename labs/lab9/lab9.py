b56c4f48219e6a47f06202ff8ae48b6a2d6ee0fd
BryceWren <wrenba@g.cofc.edu>
2022-03-28 21:43:03 -0400

    Completed lab 9

--- labs/lab9/lab9.py
+++ labs/lab9/lab9.py
@@ -1,11 +1,12 @@
 """
-Name: <your name goes here – first and last>
-<ProgramName>.py
+Name: <Bryce Wren>
+<tictactoe>.py
 """
 
 
 def build_board():
-    pass
+    board = [1, 2, 3, 4, 5, 6, 7, 8, 9]
+    return board
 
 
 def print_board(board):
@@ -37,31 +38,85 @@ def print_board(board):
 
 
 def is_legal(board, position):
-    pass
+    index_board = board[position - 1]
+    return str(index_board).isnumeric()
 
 
 def fill_spot(board, position, character):
-    pass
+    board = board
+    character = character.strip().lower()
+    board[position - 1] = character
 
 
 def winning_game(board):
-    pass
+    for i in range(0, 8, 3):
+        horizontal = (board[i] == board[i + 1]) and (board[i + 1] == board[i + 2])
+        if horizontal:
+            return True
+    for i in range(0, 3):
+        vertical = (board[i] == board[i + 3]) and (board[i + 3] == board[i + 6])
+        if vertical:
+            return True
+    if (board[0] == board[4]) and (board[4] == board[8]):
+        return True
+    if (board[2] == board[4]) and (board[4] == board[6]):
+        return True
+    return False
 
 
 def game_over(board):
-    pass
+    if winning_game(board):
+        return True
+    for position in range(1, 9):
+        if is_legal(board, position):
+            return False
+    return True
 
 
 def get_winner(board):
-    pass
+    if winning_game(board):
+        x_count = 0
+        o_count = 0
+        for position in board:
+            if position == 'x':
+                x_count += 1
+            elif position == 'o':
+                o_count += 1
+        if x_count == o_count:
+            return 'o'
+        else:
+            return 'x'
+    else:
+        return None
 
 
 def play(board):
-    pass
+    character = "x"
+    while not game_over(board):
+        print_board(board)
+        user_position = eval(input("{}'s, choose a position:".format(character)))
+        while not is_legal(board, user_position):
+            user_position = eval(input("{}'s, choose a position:".format(character)))
+        fill_spot(board, user_position, character)
+        if character == "x":
+            character = 'o'
+        else:
+            character = "x"
+
+    if winning_game(board):
+        winner = get_winner(board)
+        print("{} win".format(winner))
+    else:
+        print("Tie")
 
 
 def main():
-    pass
+    while True:
+        board = build_board()
+        play(board)
+        user_type = input('play again?')
+        if user_type[0] != 'y':
+            break
 
 
 if __name__ == '__main__':
