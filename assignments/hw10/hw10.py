114e7b11bd516578dddd5d1edc79a402a137c547
BryceWren <wrenba@g.cofc.edu>
2022-04-05 19:11:20 -0400

    Working through hw.10

--- assignments/hw10/hw10.py
+++ assignments/hw10/hw10.py
@@ -3,13 +3,44 @@ Name: <Bryce Wren>
 Assignment description:
 """
 
+def fibonacci(n):
+    n_one = 0
+    n_two = 1
+    count = 0
+    if n <= 0:
+        return None
+    else:
+        while count < n:
+            new_one = n_one + n_two
+            n_one = n_two
+            n_two = new_one
+            count += 1
+        return n_one
 
-mod = 2 % 3
-print(mod)
-#def fibonacci(n):
-#my_list = []
-acc = 1
-while True:
-    mod = 1 + acc % 2
-    thing = 1 + mod
-    print(thing)
+def double_investment(principle, rate):
+    acc = 0
+    annual = 0
+    while annual <= (principle):
+        acc += 1
+        annual_intrest = (principle * (1 + rate)) - principle
+        annual += annual_intrest
+        print(annual)
+    return acc
+
+#double_investment(26319, .18)
+
+def syracuse(n):
+    my_list = [n]
+    while n != 1:
+        if n % 2 == 0:
+            n = n/2
+            my_list.append(n)
+        else:
+            n = 3 * n + 1
+            my_list.append(n)
+    return my_list
+
+def goldbach(n):
+
+    if n % 2 == 0:
+
