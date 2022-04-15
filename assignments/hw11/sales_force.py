95726b1102615087355705cd7a5937d4c5a38e7a
BryceWren <wrenba@g.cofc.edu>
2022-04-15 19:12:37 -0400

    working through assignment 11

--- assignments/hw11/sales_force.py
+++ assignments/hw11/sales_force.py
@@ -6,11 +6,23 @@ class SalesForce:
     def add_data(self, file_name):
         file = open(file_name, "r")
         read_file = file.readline()
-        for line in read_file:
-            self.sales_people.append(line)
+        for line in read_file.split(","):
+            id = line[0]
+            name = line[1]
+            temp_person = SalesPerson(id, name)
+            sales = line[2].split()
+            temp_person.enter_sale(sales)
+            self.sales_people.append(temp_person)
 
     def quota_report(self, quota):
-        pass
+        my_list = []
+        for i in self.sales_people:
+            id = i.get_id
+            name = i.get_name
+            total_sales = i.total_sales
+            quota_met = i.met_quota(quota)
+            whole_list = [id, name, total_sales, quota_met]
+            my_list.append(whole_list)
 
     def top_seller(self):
         pass
