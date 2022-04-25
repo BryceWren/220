2f5b631e8a72157ca5f79d6b07598eb0096d4548
BryceWren <wrenba@g.cofc.edu>
2022-04-25 15:38:51 -0400

    LAB 13

--- labs/lab12/algorithms.py
+++ labs/lab12/algorithms.py
@@ -2,6 +2,8 @@
 LINEAR SEARCH
 """
 
+from graphics import *
+
 def read_data(filename):
     open_file = open(filename, "r")
     read_file = open_file.readline()
@@ -12,6 +14,7 @@ def read_data(filename):
         while split_line:
             my_list.append(split_line[acc])
             acc += 1
+    return my_list
         # for each line i need to change it to a integer or float
 
 def is_in_linear(search_val, values):
@@ -24,3 +27,85 @@ def is_in_linear(search_val, values):
         else:
             i += 1
             return found
+
+def is_in_binary(search_val, values):
+    low = 0
+    high = len(values) - 1
+    mid = 0
+    while low <= high:
+
+        mid = (high + low) // 2
+
+        if values[mid] < search_val:
+            low = mid + 1
+        elif values[mid] > search_val:
+            high = mid - 1
+        else:
+            return True
+
+    return False
+
+
+def selection_sort(values):
+    for i in range(len(values)):
+        min_val = i
+        for j in range(i, len(values)):
+            if values[j] < values[min_val]:
+                min_val = j
+        if min_val != i:
+            temp = values[i]
+            values[i] = values[min_val]
+            values[min_val] = temp
+
+
+
+def calculate_area(rectangle):
+    x1 = rectangle.getP1().getX()
+    x2 = rectangle.getP2().getX()
+    y1 = rectangle.getP1().getY()
+    y2 = rectangle.getP2().getY()
+    length = x1 - x2
+    width = y1 - y2
+    return abs(length) * abs(width)
+
+
+def rect_sort(rectangles):
+    for i in range(len(rectangles)):
+        min_val = rectangles[i]
+
+        for j in range(i, len(rectangles)):
+            rect_index = rectangles[j]
+            area1 = calculate_area(min_val)
+            area2 = calculate_area(rect_index)
+
+            if area2 < area1:
+                min_val = rectangles[j]
+
+        if min_val != i:
+            temp = rectangles[i]
+            rectangles[i] = rectangles[min_val]
+            rectangles[min_val] = temp
+
+
+
+
+def trade_alert(filename):
+    open_file = open(filename, "r")
+    read_file = open_file.read()
+    data = read_file.split(" ")
+    for i in range(len(data)):
+        if int(data[i]) > 830:
+            print("ALERT VOLUME EXCEEDS AT".format(i + 1))
+        elif int(data[i]) == 500:
+            print("pay attention".format(i + 1))
+
+    open_file.close()
+
+
+def main():
+    list1 = [1, 4, 3, 17, 22, 21]
+    print(list1)
+    selection_sort(list1)
+    print(list1)
+    trade_alert("trades.txt")
+main()
---
+++ labs/lab13/lab13.py
@@ -0,0 +1,9 @@
+from algorithms import *
+def main():
+    list1 = [1, 4, 3, 17, 22, 21]
+    print(list1)
+    selection_sort(list1)
+    print(list1)
+    trade_alert("trades.txt")
+
+main()
